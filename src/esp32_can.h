9eb4ac9554868d86372749e0b3b59be765318221
Nishanth Samala <hey@outlandnish.com>
2024-03-15 21:44:37 -0500

    fix support for mcp external controller

--- src/esp32_can.h
+++ src/esp32_can.h
@@ -1,13 +1,24 @@
 #include "esp32_can_builtin.h"
+
+// #define HAS_EXTERNAL_TWAI_CONTROLLER
+
+#if defined(USES_EXTERNAL_TWAI_CONTROLLER)
 #include "mcp2517fd.h"  //uncomment if you've got a mcp2517fd attached to spi
 //#include "mcp2515.h" //uncomment if you've got a MCP2515 attached to SPI
+#endif
 
 extern ESP32CAN CAN0;
 //Select which external chip you've got connected
 
 #if SOC_TWAI_CONTROLLER_NUM == 2
 extern ESP32CAN CAN1;
-#else
+
+#if defined(USES_EXTERNAL_TWAI_CONTROLLER)
+extern MCP2517FD CAN2;
+// extern MCP2515 CAN2;
+#endif
+
+#elif defined (USES_EXTERNAL_TWAI_CONTROLLER)
 extern MCP2517FD CAN1;
 //extern MCP2515 CAN1;
 #endif
@@ -16,4 +27,11 @@ extern volatile uint32_t biIntsCounter;
 extern volatile uint32_t biReadFrames;
 
 #define Can0 CAN0
+
+#if (SOC_TWAI_CONTROLLER_NUM == 2) or defined (USES_EXTERNAL_TWAI_CONTROLLER)
 #define Can1 CAN1
+#endif
+
+#if (SOC_TWAI_CONTROLLER_NUM == 2) and defined (USES_EXTERNAL_TWAI_CONTROLLER)
+#define Can2 CAN2
+#endif
